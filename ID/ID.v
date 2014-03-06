d341048bdfceb352706fb74467b7a96d7158a2bb
David Mateo <mateo@wisc.edu>
2014-03-06 04:02:27 -0600

    Starting on ID.v, laying out the input/output signals.

--- ID/ID.v
+++ ID/ID.v
@@ -1,3 +1,14 @@
-module ID();
+// Author:  David Mateo, R. Scott Carson
+// Instruction Decode Stage
+// This module is the top level module representing the ID stage.
+module ID(
+	// Output
+	p0, p1, shamt, func, src1sel, hlt,
+	// Input
+	instr, clk, rst_n, zr, dst);
+
+
+
+
 
 endmodule
