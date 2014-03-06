e9b980e68e1e645590bb86d76d58f34c1113db20
David Mateo <mateo@wisc.edu>
2014-03-06 03:53:41 -0600

    Made some of the top level modules fit better together, Changed filenames to match module names.

--- IF/IF_tb.v
+++ IF/IF_tb.v
@@ -1,3 +1,6 @@
+// Author:  David Mateo
+// Instruction Fetch Test Bench
+// This module contains code to functionally test the Instruction Fetch stage.
 module IF_tb();
 
 output wire [15:0] instr_mon;
---
+++ IF/PC.v
@@ -0,0 +1,27 @@
+// program_counter
+// Authors:  David Mateo, R. Scott Carson
+module PC(iaddr, clk, rst_n, hlt);
+
+output reg [15:0] iaddr;
+input clk, rst_n, hlt;
+
+wire [15:0] next_iaddr;
+
+// Sequential Logic
+///////////////////////////////////////////////
+always @(posedge clk, negedge rst_n) begin
+	if (!rst_n) begin
+		iaddr <= next_iaddr;
+	end
+	else begin
+		iaddr <= iaddr;
+	end
+end
+///////////////////////////////////////////////
+
+// Combinational Logic
+///////////////////////////////////////////////
+assign next_iaddr = (hlt) ? iaddr : iaddr + 1'b1;
+//////////////////////////////////////////////
+
+endmodule
