4584d84e3f1857b3d4ee0682e2b116a0e7034aa2
David Mateo <mateo@wisc.edu>
2014-04-16 03:57:07 -0500

    Moved alt_pc_ctrl to the EX stage from the MEM stage.

--- EX/EX.v
+++ EX/EX.v
@@ -1,12 +1,13 @@
 // Author:  David Mateo
 // Execution Stage
 // CS/ECE 552, Spring 2014
-module EX(dst, N, Z, V, alt_pc, clk, rst_n, func, shamt, src1sel, p0, imm8, p1, sdata, instr);
+module EX(dst, N, Z, V, alt_pc, alt_pc_ctrl, clk, rst_n, func, shamt, src1sel, p0, imm8, p1, sdata, instr);
 
 output wire [15:0] dst;
 output wire N, Z, V;
 output wire [15:0] sdata;
 output wire [15:0] alt_pc;
+output wire alt_pc_ctrl;
 
 input wire clk, rst_n;
 input wire [15:0] p0, p1, instr;
@@ -38,7 +39,6 @@ ALU arithmetic_logic_unit(
 	.ops(func),
 	.src1(src1_lcl),
 	.src0(p0),
-	//.ov(<not hooked up>),
 	.shamt(shamt),
 	.clk(clk),
 	.rst_n(rst_n)
@@ -50,6 +50,17 @@ alt_pc_calc AP(
 	.jump_reg(p0), 
 	// Output
 	.alt_pc(alt_pc)
+	);
+
+alt_pc_ctrl APC(
+	// Input
+	.opcode(instr[15:12]),
+	.br_cond(instr[11:9]),
+	.N(N),
+	.Z(Z),
+	.V(V),
+	// Output
+	.alt_pc_ctrl(alt_pc_ctrl)
 	);
 
 assign sdata = p1;
