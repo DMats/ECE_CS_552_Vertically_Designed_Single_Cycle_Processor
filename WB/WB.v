f2f539488372dfa13a88bc0b56d677168af7b137
David Mateo <mateo@wisc.edu>
2014-04-17 21:13:30 -0500

    Pretty sure the Pipeline is Solid.  Next I'll add in forwarding and stalling.

--- WB/WB.v
+++ WB/WB.v
@@ -3,10 +3,8 @@ module WB(
 	wb_sel, 
 	rd_data, 
 	alu_result,
-	instr, 
 	// Output
-	wb_data,
-	wb_addr);
+	wb_data);
 
 input wire wb_sel;
 input wire [15:0] rd_data, alu_result;
@@ -14,6 +12,5 @@ input wire [15:0] rd_data, alu_result;
 output wire [15:0] wb_data;
 
 assign wb_data = (wb_sel) ? alu_result : rd_data;
-assign wb_addr = instr[11:8];
 
 endmodule
